b8e0bd59279a387984a6d69fb514dbeb7d159548
Cesar Augusto <cesinhalius@hotmail.com>
2024-06-05 16:52:05 -0300

    Utilizando o pacote http

--- Primeira_API/app.js
+++ Primeira_API/app.js
@@ -1,1 +1,27 @@
-console.log("Ola Mundo");
+const http = require('http');
+
+const servidor = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain; charset=utf-8')
+    res.end('Olá, TIC em trilha')
+});
+
+const porta = 3000;
+const host = 'localhost';
+
+servidor.listen(porta,host, () => {
+    console.log(`Servidor esta executando em http://${host}:${porta}`);
+})
+
+
+function exemploTradicional(){
+    console.log('Tradicional')
+};
+
+const exemploExpressao = function(){
+    console.log('Expressao')
+};
+
+const exemploArrow = () => {
+    console.log('Arrow')
+};
