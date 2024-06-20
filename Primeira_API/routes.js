f90a4c87a5a7fd239d047d2741fae9a58e8f9489
Cesar Augusto <cesinhalius@hotmail.com>
2024-06-20 17:36:41 -0300

    Adicionando rotas de remoção e atualização de arquivos

--- Primeira_API/routes.js
+++ Primeira_API/routes.js
@@ -14,6 +14,9 @@ export default function rotas(req, res, dado){
 
         return;
     }
+
+    //Criação de arquivo
+
     if(req.method === 'PUT' && req.url === '/arquivos'){
         const body = [];
         
@@ -82,6 +85,202 @@ export default function rotas(req, res, dado){
             return;
         })
         return;
+    }
+
+    //Atualização do arquivo
+
+    if(req.method === 'PATCH' && req.url === '/arquivos'){
+        const body = [];
+        
+        req.on('data', (parte) => {
+            body.push(parte);
+        });
+
+        req.on('end', () =>{
+
+            const arquivo = JSON.parse(body);
+            
+            res.statusCode = 400;
+
+            if(!arquivo?.nome){
+                const resposta = {
+                    erro: {
+                        mensagem: `O atributo 'nome' não foi encontrado, porém é obrigatório para atualização do arquivo` 
+                    }
+                };
+
+                res.end(JSON.stringify(resposta));
+
+                return;
+            }
+
+            if(!arquivo?.conteudo){
+                const resposta = {
+                    erro: {
+                        mensagem: `O atributo 'conteudo' não foi encontrado, porém é obrigatório para atualização do arquivo` 
+                    }
+                };
+
+                res.end(JSON.stringify(resposta));
+
+                return;
+            }
+
+            fs.access(`${arquivo.nome}.txt`, fs.constants.W_OK, (erro) => {
+                if(erro){
+                    console.log('Falha ao acessar arquivo', erro);
+
+                    res.statusCode = erro.code === 'ENOENT' ? 404 : 403;
+
+                    const resposta = {
+                        erro:{
+                            mensagem: `Falha ao acessar arquivo ${arquivo.nome}`
+                        }
+                    };
+
+                    res.end(JSON.stringify(resposta));
+
+                    return;
+                }
+                fs.appendFile(`${arquivo.nome}.txt`, `\n${arquivo.conteudo}`, 'utf-8', (erro) => {
+                    if(erro){
+                        console.log('Falha ao atualizar o arquivo', erro);
+    
+                        res.statusCode = 500;
+    
+                        const resposta = {
+                            erro: {
+                                mensagem: `Falha ao atualizar o arquivo ${arquivo.nome}` 
+                            }
+                        };
+    
+                        res.end(JSON.stringify(resposta))
+                        return;
+                    }
+    
+                    res.statusCode = 200;
+    
+                    const resposta = {
+                        mensagem: `Arquivo ${arquivo.nome} atualizado com sucesso.`
+                    }
+    
+                    res.end(JSON.stringify(resposta));
+    
+                    return;
+                });
+
+            });
+
+            
+            return;
+        })
+        req.on('error', (erro) => {
+            console.log('Falha ao processar a requisição.', erro);
+
+            res.statusCode = 400;
+
+            const resposta = {
+                erro: {
+                    mensagem:'Falha ao processar a requisição'
+                }
+            };
+            res.end(JSON.stringify(resposta));
+
+            return;
+        })
+        return;
+    
+    }
+    if(req.method === 'DELETE' && req.url === '/arquivos'){
+        const body = [];
+        
+        req.on('data', (parte) => {
+            body.push(parte);
+        });
+
+        req.on('end', () =>{
+
+            const arquivo = JSON.parse(body);
+            
+            res.statusCode = 400;
+
+            if(!arquivo?.nome){
+                const resposta = {
+                    erro: {
+                        mensagem: `O atributo 'nome' não foi encontrado, porém é obrigatório para atualização do arquivo` 
+                    }
+                };
+
+                res.end(JSON.stringify(resposta));
+
+                return;
+            }
+
+        
+            fs.access(`${arquivo.nome}.txt`, fs.constants.W_OK, (erro) => {
+                if(erro){
+                    console.log('Falha ao acessar arquivo', erro);
+
+                    res.statusCode = erro.code === 'ENOENT' ? 404 : 403;
+
+                    const resposta = {
+                        erro:{
+                            mensagem: `Falha ao acessar arquivo ${arquivo.nome}`
+                        }
+                    };
+
+                    res.end(JSON.stringify(resposta));
+
+                    return;
+                }
+                fs.rm(`${arquivo.nome}.txt`, (erro) => {
+                    if(erro){
+                        console.log('Falha ao remover o arquivo', erro);
+    
+                        res.statusCode = 500;
+    
+                        const resposta = {
+                            erro: {
+                                mensagem: `Falha ao remover o arquivo ${arquivo.nome}` 
+                            }
+                        };
+    
+                        res.end(JSON.stringify(resposta))
+                        return;
+                    }
+    
+                    res.statusCode = 200;
+    
+                    const resposta = {
+                        mensagem: `Arquivo ${arquivo.nome} removido com sucesso.`
+                    }
+    
+                    res.end(JSON.stringify(resposta));
+    
+                    return;
+                });
+
+            });
+
+            
+            return;
+        })
+        req.on('error', (erro) => {
+            console.log('Falha ao processar a requisição.', erro);
+
+            res.statusCode = 400;
+
+            const resposta = {
+                erro: {
+                    mensagem:'Falha ao processar a requisição'
+                }
+            };
+            res.end(JSON.stringify(resposta));
+
+            return;
+        })
+        return;
+    
     }
 
     res.statusCode = 404;
