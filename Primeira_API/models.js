90ba1ba848b06cd24af0b2c4f3cb6e4f20d1c8bd
Cesar Augusto <cesinhalius@hotmail.com>
2024-06-24 19:17:09 -0300

    Adicionando parâmetro de não repetição

--- Primeira_API/models.js
+++ Primeira_API/models.js
@@ -17,7 +17,8 @@ export const Produto = sequelize.define('produto', {
     },
     nome:{
         type:Sequelize.STRING,
-        allowNull:false
+        allowNull:false,
+        unique: true
     },
     preco: {
         type: Sequelize.DOUBLE,
@@ -25,3 +26,24 @@ export const Produto = sequelize.define('produto', {
     }
 
 });
+
+
+export async function criaProduto(produto){
+    try{
+        await Produto.create(produto);
+        console.log(`O produto ${produto.nome} foi criado com sucesso!`);
+    }catch(erro){
+        console.log('Erro ao criar produto', erro);
+    }
+    
+}
+
+export async function listaProduto(){
+    try{
+        const itens = await Produto.findAll();
+        console.log(`Produtos listados com sucesso!`, itens);
+    }catch(erro){
+        console.log('Erro ao ler produto', erro);
+    }
+    
+}
